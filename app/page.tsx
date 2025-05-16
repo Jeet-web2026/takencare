9a58ef3d796735b2cd47b1369177f679139a95fc
Jeet <jeetnath2110@gmail.com>
2025-05-16 07:34:09 +0530

    Some modifications done

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,28 @@
+"use client"
+import { useState } from "react";
 import Image from "next/image";
 import {
   Menubar,
-  MenubarCheckboxItem,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
   MenubarRadioGroup,
   MenubarRadioItem,
   MenubarSeparator,
-  MenubarShortcut,
-  MenubarSub,
-  MenubarSubContent,
-  MenubarSubTrigger,
   MenubarTrigger,
 } from "@/components/ui/menubar"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
 
 
 export default function Home() {
@@ -46,81 +55,19 @@ export default function Home() {
           </a>
           <button className="rounded-full bg-[#017EFF] px-4 py-2 cursor-pointer text-white shadow-sm capitalize text-base">book appointment now</button>
         </div>
-        <div className="absolute bg-[#017EFF] px-5 py-3 text-white -bottom-5 w-[91%] rounded-lg">
+        <div className="absolute bg-[#017EFF] px-5 py-3 text-white -bottom-6 w-[91%] rounded-lg flex flex-row justify-between items-center">
           <Menubar>
             <MenubarMenu>
               <MenubarTrigger>Home</MenubarTrigger>
-              <MenubarContent>
-                <MenubarItem>
-                  New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem>
-                  New Window <MenubarShortcut>⌘N</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem disabled>New Incognito Window</MenubarItem>
-                <MenubarSeparator />
-                <MenubarSub>
-                  <MenubarSubTrigger>Share</MenubarSubTrigger>
-                  <MenubarSubContent>
-                    <MenubarItem>Email link</MenubarItem>
-                    <MenubarItem>Messages</MenubarItem>
-                    <MenubarItem>Notes</MenubarItem>
-                  </MenubarSubContent>
-                </MenubarSub>
-                <MenubarSeparator />
-                <MenubarItem>
-                  Print... <MenubarShortcut>⌘P</MenubarShortcut>
-                </MenubarItem>
-              </MenubarContent>
             </MenubarMenu>
             <MenubarMenu>
-              <MenubarTrigger>Edit</MenubarTrigger>
-              <MenubarContent>
-                <MenubarItem>
-                  Undo <MenubarShortcut>⌘Z</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem>
-                  Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-                </MenubarItem>
-                <MenubarSeparator />
-                <MenubarSub>
-                  <MenubarSubTrigger>Find</MenubarSubTrigger>
-                  <MenubarSubContent>
-                    <MenubarItem>Search the web</MenubarItem>
-                    <MenubarSeparator />
-                    <MenubarItem>Find...</MenubarItem>
-                    <MenubarItem>Find Next</MenubarItem>
-                    <MenubarItem>Find Previous</MenubarItem>
-                  </MenubarSubContent>
-                </MenubarSub>
-                <MenubarSeparator />
-                <MenubarItem>Cut</MenubarItem>
-                <MenubarItem>Copy</MenubarItem>
-                <MenubarItem>Paste</MenubarItem>
-              </MenubarContent>
+              <MenubarTrigger>about us</MenubarTrigger>
             </MenubarMenu>
             <MenubarMenu>
-              <MenubarTrigger>View</MenubarTrigger>
-              <MenubarContent>
-                <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
-                <MenubarCheckboxItem checked>
-                  Always Show Full URLs
-                </MenubarCheckboxItem>
-                <MenubarSeparator />
-                <MenubarItem inset>
-                  Reload <MenubarShortcut>⌘R</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem disabled inset>
-                  Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
-                </MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem inset>Toggle Fullscreen</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem inset>Hide Sidebar</MenubarItem>
-              </MenubarContent>
+              <MenubarTrigger>department</MenubarTrigger>
             </MenubarMenu>
             <MenubarMenu>
-              <MenubarTrigger>Profiles</MenubarTrigger>
+              <MenubarTrigger>pages<i className="bi bi-plus-lg ms-2 font-bold"></i></MenubarTrigger>
               <MenubarContent>
                 <MenubarRadioGroup value="benoit">
                   <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
@@ -133,7 +80,41 @@ export default function Home() {
                 <MenubarItem inset>Add Profile...</MenubarItem>
               </MenubarContent>
             </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>blog</MenubarTrigger>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>contact</MenubarTrigger>
+            </MenubarMenu>
           </Menubar>
+          <div>
+            <a href="" className="px-[10px] py-[8px] border rounded-[50%] border-white hover:bg-white hover:text-[#017EFF] text-sm me-2 transition delay-100 duration-200 ease-in-out"><i className="bi bi-linkedin"></i></a>
+            <a href="" className="px-[10px] py-[8px] border rounded-[50%] border-white hover:bg-white hover:text-[#017EFF] text-sm me-2 transition delay-100 duration-200 ease-in-out"><i className="bi bi-facebook"></i></a>
+            <a href="" className="px-[10px] py-[8px] border rounded-[50%] border-white hover:bg-white hover:text-[#017EFF] text-sm me-2 transition delay-100 duration-200 ease-in-out"><i className="bi bi-twitter-x"></i></a>
+            <a href="" className="px-[10px] py-[8px] border rounded-[50%] border-white hover:bg-white hover:text-[#017EFF] text-sm transition delay-100 duration-200 ease-in-out me-4"><i className="bi bi-youtube"></i></a>
+            <AlertDialog>
+              <AlertDialogTrigger className="border rounded-[50%] border-white text-sm px-[8px] py-[5.5px] hover:bg-white hover:text-[#017EFF] text-sm transition delay-100 duration-200 ease-in-out"><i className="bi bi-search"></i></AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>search anything you want</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    <form>
+                      <input
+                        type="text"
+                        name="searchcontent"
+                        className="w-full border p-2 rounded-sm"
+                      />
+                    </form>
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+
+          </div>
         </div>
       </section>
     </div>
