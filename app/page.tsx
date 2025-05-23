0b89c0a913a7d9209d526e95366081b779ce0464
Jeet <jeetnath2110@gmail.com>
2025-05-24 00:31:29 +0530

    Ui running

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <main className="h-screen relative flex justify-center items-center">
+    <main className="h-screen relative flex justify-center items-center px-[100px] pt-15">
       <div className="absolute top-0 left-0 w-full h-[90%] bg-[#002570] z-1 rounded-b-[25%]"></div>
       <div className="w-[50%]"></div>
       <div className="w-[50%] z-30">
-        <div className="mb-3 bg-gray-500 w-65 rounded-full py-1">
-          <p className="mb-0 uppercase text-base text-white text-center"><i className="ri-service-fill me-2"></i>healthcare specialist</p>
+        <div className="mb-4 bg-gray-500 w-55 font-semibold rounded-full py-1">
+          <p className="mb-0 uppercase text-sm text-white text-center"><i className="ri-service-fill me-2"></i>healthcare specialist</p>
         </div>
-        <h1 className="capitalize text-6xl font-semibold text-white">
+        <h1 className="capitalize text-5xl font-semibold text-white mb-2">
           best caring,
         </h1>
-        <h1 className="capitalize text-6xl font-semibold text-white">
+        <h1 className="capitalize text-5xl font-semibold text-white">
           better doctors.
         </h1>
+        <p className="text-sm opacity-50 text-white mt-4">Experience the highest standard of medical care management with our comprehensive, patient-centered solutions. We specialize in streamlining healthcare services, ensuring timely treatments, accurate medical records, and efficient coordination between providers and patients.</p>
+        <div className="flex flex-row mt-5">
+          <Button asChild>
+            <Link href="/learn-more" className="capitalize">learn more</Link>
+          </Button>
+        </div>
       </div>
     </main>
   );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-[#017EFF] text-primary-foreground shadow-xs text-base rounded-full hover:bg-[#017EF8]/20",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
