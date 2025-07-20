f1b5366f15b28e4bfe2109c6f4ada3dce0b18fcc
Jeet <jeetnath2110@gmail.com>
2025-07-20 14:33:32 +0530

    Building changes

--- components/essentials/navbar.tsx
+++ components/essentials/navbar.tsx
@@ -1,26 +1,4 @@
 "use client"
-import Image from "next/image";
-import {
-    Menubar,
-    MenubarContent,
-    MenubarItem,
-    MenubarMenu,
-    MenubarRadioGroup,
-    MenubarRadioItem,
-    MenubarSeparator,
-    MenubarTrigger,
-} from "@/components/ui/menubar"
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
 
 export const Navbar = () => {
     return (
