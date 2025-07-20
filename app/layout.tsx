0ec376a75fb876138445482ac3b6797a3a8e4ff1
Jeet <jeetnath2110@gmail.com>
2025-07-20 14:28:44 +0530

    Rebuilding it

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,24 +29,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"></link>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet"></link>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.css"
-          integrity="sha512-..."
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-
-        <style>{`
-        * {
-          font-family: "Figtree", sans-serif;
-          font-optical-sizing: auto;
-        }
-      `}</style>
       </head>
       <body className={figtree.variable}>
         <ThemeProvider
