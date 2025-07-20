a9d370a3f6617a4047fa0a4894da78e7d541bd9a
Jeet <jeetnath2110@gmail.com>
2025-07-20 21:11:15 +0530

    Navbar running

--- components/essentials/navbar.tsx
+++ components/essentials/navbar.tsx
@@ -11,23 +11,35 @@ import {
     MenubarTrigger,
 } from "@/components/ui/menubar"
 import { usePathname } from "next/navigation"
+import Link from "next/link"
+import { Button } from "../ui/button"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
 
 export const Navbar = () => {
     const presentpage = usePathname();
     return (
         <header className="fixed top-0 left-0 w-full shadow-md px-5 py-3 flex lg:flex-row flex-col justify-between items-center">
-            <div className="flex flex-row items-center">
+            <Link href="/" className="flex flex-row items-center">
                 <Image src="/images/logo.png" height={70} width={70} alt="main-logo" className="me-1" />
                 <div>
                     <h2 className="text-xl font-semibold col-1">TakenCare</h2>
                     <span className="text-sm capitalize opacity-75 font-medium">best healthcare management system</span>
                 </div>
-            </div>
+            </Link>
             <nav>
                 <Menubar>
                     <MenubarMenu>
-                        <MenubarTrigger className={presentpage === '/' ? `col-2` : 'col-1'}>Home</MenubarTrigger>
+                        <Link href="/">
+                            <MenubarTrigger className={presentpage === '/' ? `col-2` : 'col-1'}>Home</MenubarTrigger>
+                        </Link>
                     </MenubarMenu>
                     <MenubarMenu>
                         <MenubarTrigger className={presentpage === '/pages' ? `col-2` : 'col-1'}>Pages<i className="ri-add-line ms-2 text-sm"></i></MenubarTrigger>
@@ -82,8 +94,27 @@ export const Navbar = () => {
                         </MenubarContent>
                     </MenubarMenu>
                     <MenubarMenu>
-                        <MenubarTrigger className={presentpage === '/contact-us' ? `col-2` : 'col-1'}>Contact Us</MenubarTrigger>
+                        <Link href="/contact-us">
+                            <MenubarTrigger className={presentpage === '/contact-us' ? `col-2` : 'col-1'}>Contact Us</MenubarTrigger>
+                        </Link>
                     </MenubarMenu>
+                    <Button className="bg-2 cursor-pointer capitalize text-white me-2">
+                        appointment
+                    </Button>
+                    <Sheet>
+                        <SheetTrigger>
+                            <Button className="bg-1 text-lg cursor-pointer rounded-md px-3"><i className="ri-menu-unfold-line"></i></Button>
+                        </SheetTrigger>
+                        <SheetContent>
+                            <SheetHeader>
+                                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                                <SheetDescription>
+                                    This action cannot be undone. This will permanently delete your account
+                                    and remove your data from our servers.
+                                </SheetDescription>
+                            </SheetHeader>
+                        </SheetContent>
+                    </Sheet>
                 </Menubar>
             </nav>
         </header>
